9ce1d51c4ec8aefedfc1b5f38a7faec66e5de693
ahyswang <qc_9223db36a73a4e57aa346d0af9b16558@git.code.tencent.com>
2019-03-23 16:02:00 +0800

    增加动态库调用测试

---
+++ so_ldopen/math.c
@@ -0,0 +1,4 @@
+int math_add(int a, int b)
+{
+	return a + b;
+}
---
+++ so_ldopen/test_static.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "func.h"
+
+
+int main(void) 
+{
+	printf("add(10, 20) = %d\n", add(10, 20));
+	return 0;
+}
